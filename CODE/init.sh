50975b32e80e02fc94aa78a2a6c102550a78be3e
Miserial <44503693+Miserial@users.noreply.github.com>
2019-06-14 22:49:41 +0800

    官欣

---
+++ CODE/init.sh
@@ -0,0 +1,17 @@
+read path
+echo "container path is ${path}?"
+mkdir $path
+
+tar -xf rootfs.tar -C $path
+mkdir $path/proc
+
+cp /bin/bash $path/bin
+#ldd $path/bin/bash
+cp /lib/x86_64-linux-gnu/libtinfo.so.5 $path/lib
+cp /lib/x86_64-linux-gnu/libdl.so.2 $path/lib
+
+echo "rootfs,lib,Done"
+
+cp ./HTTPSVR $path
+
+echo "netdemo Done"
